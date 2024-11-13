c1a765d768fc9883e9d92627ef5ec6a422c4c8e8
hbtch <gulnaz.baembitova@mail.ru>
2024-11-13 22:16:22 +0300

    added more comments to the code

--- src/components/ToDoFilter.vue
+++ src/components/ToDoFilter.vue
@@ -1,29 +1,31 @@
 <template>
     <div>
-      <button @click="showAll">All</button>
-      <button @click="showActive">Active</button>
-      <button @click="showCompleted">Completed</button>
-    </div>
-  </template>
-  
-  <script>
-  import { mapGetters } from 'vuex';
-  
-  export default {
-    computed: {
-      ...mapGetters(['allTodos', 'activeTodos', 'completedTodos']),
-    },
-    methods: {
-      showAll() {
-        this.$emit('filter', this.allTodos);
-      },
-      showActive() {
-        this.$emit('filter', this.activeTodos);
-      },
-      showCompleted() {
-        this.$emit('filter', this.completedTodos);
-      },
-    },
-  };
-  </script>
-  
+        <button @click="showAll">All</button>
+        <button @click="showActive">Active</button>
+        <button @click="showCompleted">Completed</button>
+        </div>
+    </template>
+    
+    <script>
+// mapGetters это хелпер из vuex, упрощает доступ к геттерам vuex и связывает их с computed
+    import { mapGetters } from 'vuex';
+    
+    export default {
+        computed: {
+//  подключаем геттеры
+        ...mapGetters(['allTodos', 'activeTodos', 'completedTodos']),
+        },
+        methods: {
+        showAll() {
+            this.$emit('filter', this.allTodos);
+        },
+        showActive() {
+            this.$emit('filter', this.activeTodos);
+        },
+        showCompleted() {
+            this.$emit('filter', this.completedTodos);
+        },
+        },
+    };
+    </script>
+    
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,29 +1,29 @@
- <!-- Этот компонент отвечает за отображение одной задачи (тудушки) -->
+<!-- Этот компонент отвечает за отображение одной задачи (тудушки) -->
 <template>
     <li>
-      <label>
-        <input type="checkbox" :checked="todo.completed" @change="toggleTodo"/>
-        <span :class="{ completed: todo.completed }">{{ todo.title }}</span>
-      </label>
-    </li>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      todo: Object,
-    },
-    methods: {
-      toggleTodo() {
-        this.$emit('toggle', this.todo.id);
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .completed {
-    text-decoration: line-through;
-  }
-  </style>
-  
+        <label>
+            <input type="checkbox" :checked="todo.completed" @change="toggleTodo"/>
+            <span :class="{ completed: todo.completed }">{{ todo.title }}</span>
+        </label>
+        </li>
+    </template>
+    
+    <script>
+    export default {
+        props: {
+        todo: Object,
+        },
+        methods: {
+        toggleTodo() {
+            this.$emit('toggle', this.todo.id);
+        },
+        },
+    };
+    </script>
+    
+    <style scoped>
+    .completed {
+        text-decoration: line-through;
+    }
+    </style>
+    
